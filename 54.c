23a765d178eb994fc043359c00cc6cb8ab6b7f64
강인구 <kig2929kig@gmail.com>
2023-04-13 06:23:21 +0000

    dsfdsf

---
+++ 54.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/* 주석문
+	Data Type(자료형)
+	int => 정수
+	float => 실수
+	char => 문자
+*/
+
+int main() {
+	int num1=39;
+	float num2=3.9;
+	char munja1 = 'A';
+	char munja2[10] ="korea";
+	
+	printf("%d\n", num1);
+	printf("%f\n", num2);
+	printf("%c\n", munja1);
+	printf("%s\n", munja2);
+	
+	return 0;
+}
